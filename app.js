290a57d23214b62ff236a4033ec2ac571fc1158d
Ruarai Kirk <rkirk@digitalstack.io>
2025-02-19 12:02:11 +1100

    Fix rotation issue
    Signed-off-by: Ruarai Kirk <rkirk@digitalstack.io>

--- app.js
+++ app.js
@@ -6,97 +6,20 @@ const fs = require("fs");
 
     try {
         const imageScale = 1; // 0.41;
-        const width = 720; // 1250;
-        const height = 720; // 834;
-        const imageOffsetX = 379; // -104;
-        const imageOffsetY = -17;// -168;
-        const rotation = 0;
+        const width = 1250; // 1250;
+        const height = 834; // 834;
+        const imageOffsetX = 0; // -104;
+        const imageOffsetY = 0;// -168;
+        const rotation = 89;
 
-        // // Create a transparent background
-        // const background = sharp({
-        //     create: {
-        //         width,
-        //         height,
-        //         channels: 4, // RGBA for transparency
-        //         background: { r: 0, g: 0, b: 0, alpha: 0 }, // Transparent
-        //     },
-        // });
-
-        // const backgroundMetadata = await background.metadata();
-        // console.log('✅', {
-        //     backgroundMetadata
-        // })
-
-        // // Create Overlay
-        // const overlay = await sharp("images/custom-bmw-k75-cafe-racer.jpg")
-
-
-        // const overlayMetadata = await overlay.metadata();
-        // console.log('📊', {
-        //     overlayMetadata
-        // })
-
-        // const { width: imgWidth, height: imgHeight }  = overlayMetadata;
-
-        // if (!imgWidth || !imgHeight) throw new Error("Invalid image metadata");
-            
-        //     // Apply scale
-        // const scaledWidth = Math.round(imgWidth * imageScale);
-        // const scaledHeight = Math.round(imgHeight * imageScale);
-    
-        // const resizedOverlay = await overlay
-        //     .resize(scaledWidth, scaledHeight) // Scale image
-        //     .rotate(rotation) // Rotate
-        //     .toBuffer(); // Convert to buffer for compositing
-
-        // // const overlay = await sharp("images/custom-bmw-k75-cafe-racer.jpg")
-        // //     .metadata()
-        // //     .then(({ width: imgWidth, height: imgHeight }) => {
-        // //         if (!imgWidth || !imgHeight) throw new Error("Invalid image metadata");
-            
-        // //         // Apply scale
-        // //         scaledWidth = Math.round(imgWidth * imageScale);
-        // //         scaledHeight = Math.round(imgHeight * imageScale);
-            
-        // //         return sharp("images/custom-bmw-k75-cafe-racer.jpg")
-        // //             .resize(scaledWidth, scaledHeight) // Scale image
-        // //             .rotate(rotation) // Rotate
-        // //             .toBuffer(); // Convert to buffer for compositing
-        // //     });
-
-        // // Composite the image on the transparent background
-
-        // console.log('👻', {
-        //     width,
-        //     height,
-        //     imgWidth,
-        //     imgHeight,
-        //     scaledWidth,
-        //     scaledHeight,
-        //     top: Math.floor(scaledHeight / 2), // Positioning
-        //     left: Math.floor(scaledWidth / 2),
-        // });
-
-
-        // await background
-        //     .composite([
-        //         {
-        //             input: resizedOverlay,
-        //             top: Math.floor(scaledHeight / 2) - imageOffsetY , // Positioning
-        //             left: Math.floor(scaledWidth / 2) + imageOffsetX,
-        //         },
-        //     ])
-        //     .toFormat('png')
-        //     .toFile("output/edited-custom-bmw-k75-cafe-racer.png");
-
-
-    // Transfer
+        const backgroundWidth = 2000; // 1250;
+        const backgroundHeight = 2000; // 834;
 
         // Create a transparent background
         const background = sharp({
             create: {
-                width,
-                height,
+                width: backgroundWidth,
+                height: backgroundHeight,
                 channels: 4, // RGBA for transparency
                 background: { r: 0, g: 0, b: 0, alpha: 0 }, // Transparent
             },
@@ -112,19 +35,39 @@ const fs = require("fs");
         const scaledHeight = Math.round(originalImageHeight * imageScale);
     
         // Apply scale and rotate to overlay image
+        // let resizedOverlay = overlay
+            // .resize(scaledWidth, scaledHeight) // Scale image
+            // .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
+            // .toFormat('png')
+            // .toBuffer(); // Convert to buffer for compositing
+        
+        if (rotation === 90 || rotation === 270) {
+            overlay
+                .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
+                .resize(scaledHeight, scaledWidth) // Scale image
+        } else {
+            overlay
+                .resize(scaledWidth, scaledHeight) // Scale image
+                .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
+        }
+
         const resizedOverlay = await overlay
-            .resize(scaledWidth, scaledHeight) // Scale image
-            .rotate(rotation) // Rotate
+            .toFormat('png')
             .toBuffer(); // Convert to buffer for compositing
+
+        const resizedOverlayMetadata = await await sharp(resizedOverlay).metadata();
+        const { width: resizedOverlayWidth, height: resizedOverlayHeight }  = resizedOverlayMetadata;
+        if (!resizedOverlayWidth || !resizedOverlayHeight) throw new Error("Invalid resized image metadata");
 
         await background
             .composite([
                 {
                     input: resizedOverlay,
-                    top: Math.floor(scaledHeight / 2) - imageOffsetY , // Positioning
-                    left: Math.floor(scaledWidth / 2) + imageOffsetX,
+                    top: Math.floor(backgroundHeight / 2) - Math.floor(resizedOverlayHeight / 2) - imageOffsetY , // Positioning
+                    left: Math.floor(backgroundWidth / 2) - Math.floor(resizedOverlayWidth / 2) + imageOffsetX,
                 },
             ])
+            // .extract({top: , left: , width: , height})
             .toFormat('png')
             .toFile("output/edited-custom-bmw-k75-cafe-racer.png");
     } catch (error) {
