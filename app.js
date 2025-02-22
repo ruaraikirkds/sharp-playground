e39a5f6042001f2f132a46bcc598128021aa408d
Ruarai Kirk <rkirk@digitalstack.io>
2025-02-23 09:05:38 +1100

    Tidy and align with codebase
    Signed-off-by: Ruarai Kirk <rkirk@digitalstack.io>

--- app.js
+++ app.js
@@ -2,19 +2,12 @@
 const sharp = require("sharp");
 const fs = require("fs");
 
-(async function () {
+async function processImage(input, output, imageOptions) {
     try {
-        const imageOptions = {
-          height: 600,
-          imageOffsetX: 0,
-          imageOffsetY: 0,
-          imageRotation: 0,
-          imageScale: 0.5,
-          width: 600,
-        }
+        console.time('total');
         // Get the original image
         console.time('imageObject')
-        const imageObject = await sharp("images/custom-bmw-k75-cafe-racer.jpg", { limitInputPixels: false })
+        const imageObject = await sharp(input, { limitInputPixels: false })
         console.timeEnd('imageObject')
         // Get original image metadata
         console.time('imageObjectMetadata')
@@ -69,9 +62,8 @@ const fs = require("fs");
             throw new Error('Invalid resized image metadata');
         }
 
-        // Create a transparent background with large working area
+        // Create a transparent background with large working area, using Pythagoras to determine dimensions
         const maxBackgroundDimension = Math.round(Math.sqrt(Math.pow(scaledWidth, 2) + Math.pow(scaledHeight, 2)));
-        // const maxBackgroundDimension = Math.round(Math.max(scaledWidth, scaledHeight, imageOptions.width, imageOptions.height) * 1.5);
 
         console.log('✅', {
             overlayMetadata,
@@ -135,108 +127,31 @@ const fs = require("fs");
                 width: Math.round(imageOptions.width),
             })
             .toFormat('png') 
-            .toFile("output/edited-custom-bmw-k75-cafe-racer.jpg");
+            .toFile(output);
         console.timeEnd('final')
+        console.timeEnd('total');
     } catch (error) {
         console.log('💥', {
             error
         })
     }
-    // try {
-    //     const imageScale = 1; // 0.41;
-    //     const width = 600; // 1250;
-    //     const height = 600; // 834;
-    //     const imageOffsetX = 0; // -104;
-    //     const imageOffsetY = 0;// -168;
-    //     const rotation = 89;
+};
 
-    //     const backgroundWidth = 2000; // 1250;
-    //     const backgroundHeight = 2000; // 834;
+const imageOptions = {
+    height: 600,
+    imageOffsetX: 0, // Positive right, negative left
+    imageOffsetY: 0, // Positive up, negative down
+    imageRotation: 0,
+    imageScale: 0.5, // Max 2
+    width: 600,
+  }
 
-    //     // Create a transparent background
-    //     const background = sharp({
-    //         create: {
-    //             width: backgroundWidth,
-    //             height: backgroundHeight,
-    //             channels: 4, // RGBA for transparency
-    //             background: { r: 0, g: 0, b: 0, alpha: 0 }, // Transparent
-    //         },
-    //     });
-    //     // Create Overlay
-    //     const overlay = await sharp("images/custom-bmw-k75-cafe-racer.jpg")
-    //     const overlayMetadata = await overlay.metadata();
-    //     const { width: originalImageWidth, height: originalImageHeight }  = overlayMetadata;
-    //     if (!originalImageWidth || !originalImageHeight) throw new Error("Invalid image metadata");
-            
-    //     // Apply scale
-    //     const scaledWidth = Math.round(originalImageWidth * imageScale);
-    //     const scaledHeight = Math.round(originalImageHeight * imageScale);
-    
-    //     // Apply scale and rotate to overlay image
-    //     // let resizedOverlay = overlay
-    //         // .resize(scaledWidth, scaledHeight) // Scale image
-    //         // .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
-    //         // .toFormat('png')
-    //         // .toBuffer(); // Convert to buffer for compositing
-        
-    //     if (rotation === 90 || rotation === 270) {
-    //         overlay
-    //             .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
-    //             .resize(scaledHeight, scaledWidth) // Scale image
-    //     } else {
-    //         overlay
-    //             .resize(scaledWidth, scaledHeight) // Scale image
-    //             .rotate(rotation, {background: { r: 0, g: 0, b: 0, alpha: 0 }}) // Rotate and ensure background is transparent
-    //     }
+const input = "images/custom-bmw-k75-cafe-racer.jpg"
+const output = "output/edited-custom-bmw-k75-cafe-racer.jpg"
 
-    //     const resizedOverlay = await overlay
-    //         .toFormat('png')
-    //         .toBuffer(); // Convert to buffer for compositing
-
-    //     const resizedOverlayMetadata = await await sharp(resizedOverlay).metadata();
-    //     const { width: resizedOverlayWidth, height: resizedOverlayHeight }  = resizedOverlayMetadata;
-    //     if (!resizedOverlayWidth || !resizedOverlayHeight) throw new Error("Invalid resized image metadata");
-
-    //     // await background
-    //     //     .composite([
-    //     //         {
-    //     //             input: resizedOverlay,
-    //     //             top: Math.floor(backgroundHeight / 2) - Math.floor(resizedOverlayHeight / 2) - imageOffsetY , // Positioning
-    //     //             left: Math.floor(backgroundWidth / 2) - Math.floor(resizedOverlayWidth / 2) + imageOffsetX,
-    //     //         },
-    //     //     ])
-    //     //     .raw()
-    //     //     .extract({
-    //     //         height,
-    //     //         top: Math.floor(backgroundHeight / 2) - Math.floor(height / 2), // Positioning
-    //     //         left: Math.floor(backgroundWidth / 2) - Math.floor(width / 2),
-    //     //         // left: Math.round(backgroundWidth / 2 - width / 2),
-    //     //         // top: Math.round(backgroundHeight / 2 - height / 2),
-    //     //         width,
-    //     //     })
-    //     //     .toFormat('png')
-    //     //     .toFile("output/edited-custom-bmw-k75-cafe-racer.png");
-
-    //     const preFinalSize = await background
-    //         .composite([
-    //             {
-    //                 input: resizedOverlay,
-    //                 top: Math.floor(backgroundHeight / 2) - Math.floor(resizedOverlayHeight / 2) - imageOffsetY , // Positioning
-    //                 left: Math.floor(backgroundWidth / 2) - Math.floor(resizedOverlayWidth / 2) + imageOffsetX,
-    //             },
-    //         ])
-    //         .toFormat('png')
-    //         .toBuffer()
-
-    //     await sharp(preFinalSize)
-    //         .extract({
-    //             height,
-    //             top: Math.floor(backgroundHeight / 2) - Math.floor(height / 2),
-    //             left: Math.floor(backgroundWidth / 2) - Math.floor(width / 2),
-    //             width,
-    //         })
-    //         .toFile("output/edited-custom-bmw-k75-cafe-racer.png");
-    // } catch (error) {
-    //     console.log(error);
-    // }
-})();
+// Usage
+processImage(
+    input,
+    output,
+    imageOptions
+)
